935e2ab10a3735cf6ea2f80a1c1407bec930a4a9
Johannes Schultze <schultze@effective-webwork.de>
2014-06-17 14:26:53 +0200

    Configuration.

--- bibapp/BAConfigurationHFMW.m
+++ bibapp/BAConfigurationHFMW.m
@@ -23,10 +23,13 @@
     [self.currentBibPAIAURLs addObject:[[NSArray alloc] initWithObjects:@"https://paia.gbv.de/DE-Hil2", @"Standard-Katalog", nil]];
     self.currentBibFeedURL = @"http://www.uni-weimar.de/de/universitaet/aktuell/pinnwaende/rss/bereich/bibliothek/";
     self.currentBibTintColor = [[UIColor alloc] initWithRed:0.77 green:0.18 blue:0.36 alpha:1.0];
-    self.currentBibImprintTitles = [[NSMutableArray alloc] initWithObjects:@"Anbieter", @"Gestaltung und Umsetzung", nil];
-    [self.currentBibImprint setObject:@"Universitätsbibliothek der Bauhaus-Universität Weimar\nSteubenstr. 6\n99423 Weimar\nTelefon: 03643/582801\nFax: 03643/582802\nE-Mail: info@ub.uni-weimar.de\nDie Bauhaus-Universität Weimar ist eine Körperschaft des öffentlichen Rechts. Sie wird durch ihren Rektor, Herrn Prof. Dr. Karl Beucke, gesetzlich vertreten. Zuständige Aufsichtsbehörde: Thüringer Ministerium für Bildung, Wissenschaft und Kultur." forKey:@"Anbieter"];
-    [self.currentBibImprint setObject:@"Entwurf und Konzeption\nA. Christensen (UB Lüneburg),\nJ. Schrader (UB Hildesheim),\nJ. Voss (VZG Göttingen)\n\nUmsetzung\neffective WEBWORK GmbH" forKey:@"Gestaltung und Umsetzung"];
-    self.currentBibContact = @"Fragen an die Bibliothek:\nTelefon: 03643/582801\nE-Mail: info@ub.uni-weimar.de";
+    self.currentBibImprintTitles = [[NSMutableArray alloc] initWithObjects:@"Anbieter", @"Vertreter", @"Angaben zum Datenschutz", @"Rechtliche Hinweise zur Haftung", @"Gestaltung und Umsetzung", nil];
+    [self.currentBibImprint setObject:@"" forKey:@"Anbieter"];
+    [self.currentBibImprint setObject:@"" forKey:@"Vertreter"];
+    [self.currentBibImprint setObject:@"" forKey:@"Angaben zum Datenschutz"];
+    [self.currentBibImprint setObject:@"" forKey:@"Rechtliche Hinweise zur Haftung"];
+    [self.currentBibImprint setObject:@"" forKey:@"Gestaltung und Umsetzung"];
+    self.currentBibContact = @"";
     self.currentBibLocationURI = @"http://uri.gbv.de/organization/isil/DE-Wim2";
     [self.currentBibLocationURIs addObject:[[NSArray alloc] initWithObjects:@"http://uri.gbv.de/organization/isil/DE-Wim2", @"Standard-Katalog", nil]];
     self.currentBibSearchCountURL = @"http://dbspixel.hbz-nrw.de/count?id=EM482&amp;page=2";
