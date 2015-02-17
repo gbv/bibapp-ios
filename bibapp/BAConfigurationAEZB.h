d3c17b30603870da3077bdcd43b57145a8daae70
Johannes Schultze <schultze@effective-webwork.de>
2015-02-17 13:24:52 +0100

    Configuration ÄZB.

---
+++ bibapp/BAConfigurationAEZB.h
@@ -0,0 +1,13 @@
+//
+//  BAConfigurationAEZB.h
+//  bibapp
+//
+//  Created by Johannes Schultze on 17.02.15.
+//  Copyright (c) 2015 Johannes Schultze. All rights reserved.
+//
+
+#import "BAConfiguration.h"
+
+@interface BAConfigurationAEZB : BAConfiguration
+
+@end
---
+++ bibapp/BAConfigurationAEZB.m
@@ -0,0 +1,37 @@
+//
+//  BAConfigurationAEZB.m
+//  bibapp
+//
+//  Created by Johannes Schultze on 17.02.15.
+//  Copyright (c) 2015 Johannes Schultze. All rights reserved.
+//
+
+#import "BAConfigurationAEZB.h"
+
+@implementation BAConfigurationAEZB
+
+- (void)initConfiguration
+{
+   self.currentBibSearchMaximumRecords = @"20";
+   self.currentBibLocalSearchURL = @"opac-de";
+   [self.currentBibLocalSearchURLs addObject:[[NSArray alloc] initWithObjects:@"opac-de", @"Standard-Katalog", @"Lokale Suche", @"Lokale Suche", nil]];
+   self.currentBibDetailURL = @"http://daia.gbv.de/isil/DE";
+   [self.currentBibDetailURLs addObject:[[NSArray alloc] initWithObjects:@"http://daia.gbv.de/isil/DE", @"Standard-Katalog", nil]];
+   self.currentBibPAIAURL = @"https://paia.gbv.de/DE-84";
+   [self.currentBibPAIAURLs addObject:[[NSArray alloc] initWithObjects:@"https://paia.gbv.de/DE", @"Standard-Katalog", nil]];
+   self.currentBibFeedURL = @"";
+   self.currentBibTintColor = [[UIColor alloc] initWithRed:0.74 green:0.11 blue:0.23 alpha:1.0];
+   self.currentBibImprintTitles = [[NSMutableArray alloc] initWithObjects:@"Anbieter", @"Vertreter", @"Angaben zum Datenschutz", @"Rechtliche Hinweise zur Haftung", @"Gestaltung und Umsetzung", nil];
+   [self.currentBibImprint setObject:@"" forKey:@"Anbieter"];
+   [self.currentBibImprint setObject:@"" forKey:@"Vertreter"];
+   [self.currentBibImprint setObject:@"" forKey:@"Angaben zum Datenschutz"];
+   [self.currentBibImprint setObject:@"" forKey:@"Rechtliche Hinweise zur Haftung"];
+   [self.currentBibImprint setObject:@"Ursprünglicher Entwurf und Konzeption: UB Lüneburg, UB Hildesheim, VZG Göttingen\n\nAnpassung: UB Braunschweig\n\nUmsetzung: effective WEBWORK GmbH" forKey:@"Gestaltung und Umsetzung"];
+   self.currentBibContact = @"";
+   self.currentBibLocationURI = @"http://uri.gbv.de/organization/isil/DE";
+   [self.currentBibLocationURIs addObject:[[NSArray alloc] initWithObjects:@"http://uri.gbv.de/organization/isil/DE", @"Standard-Katalog", nil]];
+   self.currentBibSearchCountURL = @"";
+   self.currentBibUsePAIAWrapper = YES;
+}
+
+@end
