19d321747725c8e81a486bdb7c63b6cef1553170
Johannes Schultze <schultze@effective-webwork.de>
2015-03-26 16:19:42 +0100

    AEZB.

--- bibapp/BAConfigurationAEZB.m
+++ bibapp/BAConfigurationAEZB.m
@@ -16,19 +16,21 @@
    [self.currentBibLocalSearchURLs addObject:[[NSArray alloc] initWithObjects:@"opac-de-18-64", @"Standard-Katalog", @"Lokale Suche", @"Lokale Suche", nil]];
    [self.currentBibDetailURLs addObject:[[NSArray alloc] initWithObjects:@"http://daia.gbv.de/isil/DE-18", @"Standard-Katalog", nil]];
    [self.currentBibPAIAURLs addObject:[[NSArray alloc] initWithObjects:@"https://aezb.api.effective-webwork.de", @"Standard-Katalog", nil]];
-   self.currentBibFeedURL = @"";
-   self.currentBibTintColor = [[UIColor alloc] initWithRed:0.74 green:0.11 blue:0.23 alpha:1.0];
+   self.currentBibFeedURL = @"http://www.uke.de/aerztliche-zentralbibliothek";
+   [self.currentBibFeedURLs addObject:[[NSArray alloc] initWithObjects:@"http://www.uke.de/aerztliche-zentralbibliothek", @"Standard-Katalog", nil]];
+   self.currentBibTintColor = [UIColor colorWithRed:0 green:0.18 blue:0.475 alpha:1];
    self.currentBibImprintTitles = [[NSMutableArray alloc] initWithObjects:@"Anbieter", @"Vertreter", @"Angaben zum Datenschutz", @"Rechtliche Hinweise zur Haftung", @"Gestaltung und Umsetzung", nil];
    [self.currentBibImprint setObject:@"" forKey:@"Anbieter"];
    [self.currentBibImprint setObject:@"" forKey:@"Vertreter"];
    [self.currentBibImprint setObject:@"" forKey:@"Angaben zum Datenschutz"];
    [self.currentBibImprint setObject:@"" forKey:@"Rechtliche Hinweise zur Haftung"];
    [self.currentBibImprint setObject:@"Ursprünglicher Entwurf und Konzeption: UB Lüneburg, UB Hildesheim, VZG Göttingen\n\nUmsetzung: effective WEBWORK GmbH" forKey:@"Gestaltung und Umsetzung"];
-   self.currentBibContact = @"";
+   self.currentBibContact = @"Ärztliche Zentralbibliothek\n\nTäglich geöffnet von 08.00-22.00 Uhr\nService Mo.-Fr. von 09.00-17.00 Uhr";
    self.currentBibLocationURI = @"http://uri.gbv.de/organization/isil/DE-18-64";
    [self.currentBibLocationURIs addObject:[[NSArray alloc] initWithObjects:@"http://uri.gbv.de/organization/isil/DE-18-64", @"Standard-Katalog", nil]];
    self.currentBibSearchCountURL = @"";
    self.currentBibRequestTitle = @"kostenpflichtig (0,80€) vormerken";
+   self.currentBibFeedURLIsWebsite = YES;
 }
 
 @end
