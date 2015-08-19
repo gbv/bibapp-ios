a8552d5c3c799806623f5cbbfcb7bbf2617957ec
Johannes Schultze <schultze@effective-webwork.de>
2015-08-19 11:20:53 +0200

    NDH.

--- bibapp/BAConfigurationNDH.m
+++ bibapp/BAConfigurationNDH.m
@@ -19,8 +19,8 @@
     self.currentBibFeedURL = @"";
     self.currentBibTintColor = [UIColor colorWithRed:0.000000F green:0.349020F blue:0.619608F alpha:1.0F];
     self.currentBibImprintTitles = [[NSMutableArray alloc] initWithObjects:@"Anbieter", nil];
-    [self.currentBibImprint setObject:@"Fachhochschulbibliothek Nordhausen\nWeinberghof 4\n99734 Nordhausen\n\nhttp://www.fh-nordhausen.de/bibliothek.html\nE-Mail: bibliothek@fh-nordhausen.de\nTel.: 03631 420-184\nFax: 03631 420-815" forKey:@"Anbieter"];
-    self.currentBibContact = @"Fachhochschule Nordhausen\nHochschulbibliothek\nWeinberghof 4\n99734 Nordhausen\n\nTel. Information: 03631 420-184\nTel. Ausleihe: 03631 420-185\nFax: 03631 420-815\nE-Mail: bibliothek@fh-nordhausen.de";
+    [self.currentBibImprint setObject:@"Hochschulbibliothek Nordhausen\nWeinberghof 4\n99734 Nordhausen\n\nhttp://www.hs-nordhausen.de/service/bibliothek/\nE-Mail: bibliothek@hs-nordhausen.de\nTel.: 03631 420-184\nFax: 03631 420-815\n\nImpressum der Hochschule:\nhttp://www.hs-nordhausen.de/allgemeines/impressum/?T=0" forKey:@"Anbieter"];
+    self.currentBibContact = @"Hochschule Nordhausen\nHochschulbibliothek\nWeinberghof 4\n99734 Nordhausen\n\nTel. Information: 03631 420-184\nTel. Ausleihe: 03631 420-185\nFax: 03631 420-815\nE-Mail: bibliothek@hs-nordhausen.de";
     self.currentBibLocationURI = @"http://uri.gbv.de/organization/isil/DE-564";
     [self.currentBibLocationURIs addObject:[[NSArray alloc] initWithObjects:@"http://uri.gbv.de/organization/isil/DE-564", @"Standard-Katalog", nil]];
     self.currentBibSearchCountURL = @"";
