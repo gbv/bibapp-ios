0f8189f5555ff69c6acb0526cd3f45c35bfeade9
Johannes Schultze <schultze@effective-webwork.de>
2015-11-03 12:38:09 +0100

    Revert "NDH."
    This reverts commit cb28c164a8adcebf696b9943a8759d1519df2ecb.

--- bibapp/BAConfigurationNDH.m
+++ bibapp/BAConfigurationNDH.m
@@ -19,8 +19,8 @@
     self.currentBibFeedURL = @"";
     self.currentBibTintColor = [UIColor colorWithRed:0.000000F green:0.349020F blue:0.619608F alpha:1.0F];
     self.currentBibImprintTitles = [[NSMutableArray alloc] initWithObjects:@"Anbieter", nil];
-    [self.currentBibImprint setObject:@"Hochschule Nordhausen\nHochschulbibliothek\nWeinberghof 4\n99734 Nordhausen\n\nTel.: 03631 420-184\nFax:  03631 420-815\nE-Mail: bibliothek@hs-nordhausen.de\nHomepage: http://www.hs-nordhausen.de/service/bibliothek/\n\nÖffnungszeiten:\nMo-Do: 9:00 Uhr - 20:00 Uhr\nFr.: 9:00 Uhr - 17:00 Uhr\n\nImpressum der Hochschule:\nhttp://www.hs-nordhausen.de/allgemeines/impressum/?T=0" forKey:@"Anbieter"];
-    self.currentBibContact = @"Hochschule Nordhausen\nHochschulbibliothek\nWeinberghof 4\n99734 Nordhausen\n\nTel.: 03631 420-184\nFax:  03631 420-815\nE-Mail: bibliothek@hs-nordhausen.de\nHomepage: http://www.hs-nordhausen.de/service/bibliothek/\n\nÖffnungszeiten:\nMo-Do: 9:00 Uhr - 20:00 Uhr\nFr.: 9:00 Uhr - 17:00 Uhr";
+    [self.currentBibImprint setObject:@"Hochschulbibliothek Nordhausen\nWeinberghof 4\n99734 Nordhausen\n\nhttp://www.hs-nordhausen.de/service/bibliothek/\nE-Mail: bibliothek@hs-nordhausen.de\nTel.: 03631 420-184\nFax: 03631 420-815\n\nImpressum der Hochschule:\nhttp://www.hs-nordhausen.de/allgemeines/impressum/?T=0" forKey:@"Anbieter"];
+    self.currentBibContact = @"Hochschule Nordhausen\nHochschulbibliothek\nWeinberghof 4\n99734 Nordhausen\n\nTel. Information: 03631 420-184\nTel. Ausleihe: 03631 420-185\nFax: 03631 420-815\nE-Mail: bibliothek@hs-nordhausen.de";
     self.currentBibLocationURI = @"http://uri.gbv.de/organization/isil/DE-564";
     [self.currentBibLocationURIs addObject:[[NSArray alloc] initWithObjects:@"http://uri.gbv.de/organization/isil/DE-564", @"Standard-Katalog", nil]];
     self.currentBibSearchCountURL = @"";
