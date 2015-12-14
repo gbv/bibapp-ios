854207218ae6a2b7a9c5b8e583a5f0a37b5d936c
Johannes Schultze <schultze@effective-webwork.de>
2015-12-14 13:34:10 +0100

    CB.

--- bibapp/BAConfigurationCB.m
+++ bibapp/BAConfigurationCB.m
@@ -15,7 +15,7 @@
    self.currentBibSearchMaximumRecords = @"20";
    [self.currentBibLocalSearchURLs addObject:[[NSArray alloc] initWithObjects:@"opac-de-205", @"Standard-Katalog", @"Lokale Suche", @"Lokale Suche", nil]];
    [self.currentBibDetailURLs addObject:[[NSArray alloc] initWithObjects:@"http://daia.gbv.de/isil/DE-205", @"Standard-Katalog", nil]];
-   [self.currentBibPAIAURLs addObject:[[NSArray alloc] initWithObjects:@"https://paia.gbv.de/de-205", @"Standard-Katalog", nil]];
+   [self.currentBibPAIAURLs addObject:[[NSArray alloc] initWithObjects:@"https://paia.gbv.de/DE-205", @"Standard-Katalog", nil]];
    //self.currentBibFeedURL = @"";
    self.currentBibTintColor = [[UIColor alloc] initWithRed:0.00 green:0.29 blue:0.52  alpha:1.0];
    self.currentBibImprintTitles = [[NSMutableArray alloc] initWithObjects:@"Anbieter im Sinne von § 5 TMG", @"Vertretungsberechtigte", @"Präses", @"Hauptgeschäftsführer", @"Aufsichtsbehörde", @"Angaben zum Datenschutz", @"Rechtliche Hinweise zur Haftung", @"Gestaltung und Umsetzung", nil];
