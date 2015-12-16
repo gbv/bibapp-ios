d68c29cb7c17b910f5bf878eb07f4cc0b0eddcf2
Johannes Schultze <schultze@effective-webwork.de>
2015-12-16 19:55:30 +0100

    Bugfixes.

--- bibapp/BAOptionsViewControllerPad.m
+++ bibapp/BAOptionsViewControllerPad.m
@@ -77,15 +77,14 @@
 
 - (void)handleTapBehind:(UITapGestureRecognizer *)sender
 {
-    if (sender.state == UIGestureRecognizerStateEnded) {
-        CGPoint location = [sender locationInView:nil]; //Passing nil gives us coordinates in the window
-        //Then we convert the tap's location into the local view's coordinate system, and test to see if it's in or outside. If outside, dismiss the view.
-        if (![self.view pointInside:[self.view convertPoint:location fromView:self.view.window] withEvent:nil]) {
-            // Remove the recognizer first so it's view.window is valid.
-            [self.view.window removeGestureRecognizer:sender];
-            [self dismissViewControllerAnimated:YES completion:nil];
-        }
-    }
+   if (sender.state == UIGestureRecognizerStateEnded) {
+      UIView *rootView = self.view.window.rootViewController.view;
+      CGPoint location = [sender locationInView:rootView];
+      if (![self.view pointInside:[self.view convertPoint:location fromView:rootView] withEvent:nil]) {
+         [self.view.window removeGestureRecognizer:sender];
+         [self dismissViewControllerAnimated:YES completion:nil];
+      }
+   }
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
