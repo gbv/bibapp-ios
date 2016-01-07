a3c4bc27e05ac1a8fa858f90f55637220dbd7a51
Johannes Schultze <schultze@effective-webwork.de>
2016-01-07 17:06:48 +0100

    CB.

--- bibapp/BAConfigurationCB.m
+++ bibapp/BAConfigurationCB.m
@@ -16,7 +16,7 @@
    [self.currentBibLocalSearchURLs addObject:[[NSArray alloc] initWithObjects:@"opac-de-205", @"Standard-Katalog", @"Lokale Suche", @"Lokale Suche", nil]];
    [self.currentBibDetailURLs addObject:[[NSArray alloc] initWithObjects:@"http://daia.gbv.de/isil/DE-205", @"Standard-Katalog", nil]];
    [self.currentBibPAIAURLs addObject:[[NSArray alloc] initWithObjects:@"https://paia.gbv.de/DE-205", @"Standard-Katalog", nil]];
-   //self.currentBibFeedURL = @"";
+   self.currentBibFeedURL = @"";
    self.currentBibTintColor = [[UIColor alloc] initWithRed:0.00 green:0.29 blue:0.52  alpha:1.0];
    self.currentBibImprintTitles = [[NSMutableArray alloc] initWithObjects:@"Anbieter im Sinne von § 5 TMG", @"Vertretungsberechtigte", @"Präses", @"Hauptgeschäftsführer", @"Aufsichtsbehörde", @"Angaben zum Datenschutz", @"Rechtliche Hinweise zur Haftung", @"Gestaltung und Umsetzung", nil];
    [self.currentBibImprint setObject:@"Handelskammer Hamburg\nCommerzbibliothek\nAdolphsplatz 1\n20457 Hamburg\n\nTelefon: +49 40 36138-377\nTelefax: +49 40 36138-437\nE-Mail: info@commerzbibliothek.de\n\nDie Handelskammer Hamburg ist eine Körperschaft des öffentlichen Rechts.\n\nDie Commerzbibliothek ist eine Einrichtung der Handelskammer Hamburg. Leiterin der Commerzbibliothek ist Sabine Lurtz-Herting." forKey:@"Anbieter im Sinne von § 5 TMG"];
