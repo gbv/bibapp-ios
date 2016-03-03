827767f2a32365c28c4dc6b6c3f19f7e4b944a68
Johannes Schultze <schultze@effective-webwork.de>
2016-03-03 16:13:56 +0100

    UFB.

--- bibapp/BAConfigurationUFB.m
+++ bibapp/BAConfigurationUFB.m
@@ -14,7 +14,7 @@
 {
     self.currentBibSearchMaximumRecords = @"20";
     self.currentBibStandardCatalogue = @"UFB Erfurt/Gotha";
-    [self.currentBibLocalSearchURLs addObject:[[NSArray alloc] initWithObjects:@"opac-de-1867", @"UFB Erfurt/Gotha", @"Lokale Suche", @"Lokale Suche", nil]];
+    [self.currentBibLocalSearchURLs addObject:[[NSArray alloc] initWithObjects:@"opac-de-1876", @"UFB Erfurt/Gotha", @"Lokale Suche", @"Lokale Suche", nil]];
     [self.currentBibLocalSearchURLs addObject:[[NSArray alloc] initWithObjects:@"opac-de-547", @"UB Erfurt", @"Lokale Suche", @"Lokale Suche", nil]];
     [self.currentBibLocalSearchURLs addObject:[[NSArray alloc] initWithObjects:@"opac-de-39", @"FB Gotha", @"Lokale Suche", @"Lokale Suche", nil]];
     [self.currentBibDetailURLs addObject:[[NSArray alloc] initWithObjects:@"http://daia.gbv.de/isil/DE-547", @"UFB Erfurt/Gotha", nil]];
@@ -28,8 +28,8 @@
     self.currentBibImprintTitles = [[NSMutableArray alloc] initWithObjects:@"Anbieter", nil];
     [self.currentBibImprint setObject:@"Universitäts- und Forschungsbibliothek Erfurt/Gotha\nNordhäuser Straße 63\n99089 Erfurt\nTelefon: +49 361 737-5800\nFax: +49 361 737-5849\nE-Mail: information.ub(at)uni-erfurt.de\n\nDie Universität Erfurt ist eine Körperschaft des öffentlichen Rechts.\nSie wird durch Prof. Dr. Kai Brodersen, den Präsidenten der Universität Erfurt, gesetzlich vertreten.\nUSt-Id-Nr: DE 811 627 407\nZuständige Aufsichtsbehörde\nMinisterium für Bildung, Wissenschaft und Kultur\nWerner-Seelenbinder Straße 7\n99096 Erfurt\n\nVerantwortlich für den Inhalt nach § 55 Abs. 2 RStV\nBibliotheksdirektor (komm.) Dr. Eckart Gerstner\n+49 361 737-5500\n+49 361 737-5509\neckart.gerstner(at)uni-erfurt.de" forKey:@"Anbieter"];
     self.currentBibContact = @"UB Erfurt\nE-Mail: information.ub@uni-erfurt.de\nTelefon: 0361/737-5800\n\nFB Gotha\nE-Mail: bibliothek.gotha@uni-erfurt.de\n\nTelefon: 0361/737-5540";
-    self.currentBibLocationURI = @"http://uri.gbv.de/organization/isil/DE-1867";
-    [self.currentBibLocationURIs addObject:[[NSArray alloc] initWithObjects:@"http://uri.gbv.de/organization/isil/DE-1867", @"UFB Erfurt/Gotha", nil]];
+    self.currentBibLocationURI = @"http://uri.gbv.de/organization/isil/DE-1876";
+    [self.currentBibLocationURIs addObject:[[NSArray alloc] initWithObjects:@"http://uri.gbv.de/organization/isil/DE-1876", @"UFB Erfurt/Gotha", nil]];
     [self.currentBibLocationURIs addObject:[[NSArray alloc] initWithObjects:@"http://uri.gbv.de/organization/isil/DE-547", @"UB Erfurt", nil]];
     [self.currentBibLocationURIs addObject:[[NSArray alloc] initWithObjects:@"http://uri.gbv.de/organization/isil/DE-39", @"FB Gotha", nil]];
     //self.currentBibSearchCountURL = @"http://dbspixel.hbz-nrw.de/count?id_EX461&amp;page=20";
