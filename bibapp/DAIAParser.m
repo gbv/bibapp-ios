ea3baa800039453bd8015024f776af3cfeb746e3
Johannes Schultze <schultze@effective-webwork.de>
2016-04-07 15:36:03 +0200

    DAIA2 Support.

--- bibapp/DAIAParser.m
+++ bibapp/DAIAParser.m
@@ -16,95 +16,170 @@
 
 -(void)parseDAIAForDocument:(BADocument *)baDocument WithResult:(NSObject *)result {
    [baDocument setItems:[[NSMutableArray alloc] init]];
-   GDataXMLDocument *parser = [[GDataXMLDocument alloc] initWithData:(NSData *)result options:0 error:nil];
-   NSArray *document = [parser nodesForXPath:@"_def_ns:daia/_def_ns:document" error:nil];
+   
+   NSDictionary* json = [NSJSONSerialization JSONObjectWithData:(NSData *)result options:kNilOptions error:nil];
+   
+   NSArray *document = [json objectForKey:@"document"];
    if ([document count] > 0) {
-      GDataXMLElement *tempDocument = [document objectAtIndex:0];
-      [baDocument setDocumentID:[[tempDocument attributeForName:@"id"] stringValue]];
-      [baDocument setHref:[[tempDocument attributeForName:@"href"] stringValue]];
+      if (![[[document objectAtIndex:0] objectForKey:@"id"] isEqual:[NSNull null]]) {
+         [baDocument setDocumentID:[[document objectAtIndex:0] objectForKey:@"id"]];
+      }
+      if (![[[document objectAtIndex:0] objectForKey:@"href"] isEqual:[NSNull null]]) {
+         [baDocument setHref:[[document objectAtIndex:0] objectForKey:@"href"]];
+      }
    }
    
-   NSArray *institution = [parser nodesForXPath:@"_def_ns:daia/_def_ns:institution" error:nil];
-   if ([institution count] > 0) {
-      GDataXMLElement *tempInstitution = [institution objectAtIndex:0];
-      [baDocument setInstitution:[tempInstitution stringValue]];
+   NSDictionary *institution = [json objectForKey:@"institution"];
+   if (![institution isEqual:[NSNull null]]) {
+      if (![[institution objectForKey:@"content"] isEqual:[NSNull null]]) {
+         [baDocument setInstitution:[institution objectForKey:@"content"]];
+      }
    }
    
    BAConnector *locationConnector = [BAConnector generateConnector];
-   NSArray *items = [parser nodesForXPath:@"_def_ns:daia/_def_ns:document/_def_ns:item" error:nil];
-   for (GDataXMLElement *item in items) {
+   
+   NSArray *items;
+   if ([document count] > 0) {
+      items = [[document objectAtIndex:0] objectForKey:@"item"];
+   }
+   
+   for (NSDictionary *item in items) {
       BADocumentItem *tempDocumentItem = [[BADocumentItem alloc] init];
       [tempDocumentItem setEdition:baDocument.documentID];
-      [tempDocumentItem setHref:[[item attributeForName:@"href"] stringValue]];
-      [tempDocumentItem setItemID:[[item attributeForName:@"id"] stringValue]];
-      
-      NSArray *label = [item elementsForName:@"label"];
-      if ([label count] == 1) {
-         [tempDocumentItem setLabel:[[label objectAtIndex:0] stringValue]];
+      if (![[item objectForKey:@"href"] isEqual:[NSNull null]]) {
+         [tempDocumentItem setHref:[item objectForKey:@"href"]];
+      }
+      if (![[item objectForKey:@"id"] isEqual:[NSNull null]]) {
+         [tempDocumentItem setItemID:[item objectForKey:@"id"]];
       }
       
-      NSArray *department = [item elementsForName:@"department"];
-      if ([department count] == 1) {
-         [tempDocumentItem setUri:[[[department objectAtIndex:0] attributeForName:@"id"] stringValue]];
-         [tempDocumentItem setLocation:[locationConnector loadLocationForUri:[[[department objectAtIndex:0] attributeForName:@"id"] stringValue]]];
-         if (![tempDocumentItem.location.shortname isEqualToString:@""]) {
-            [tempDocumentItem setDepartment:tempDocumentItem.location.shortname];
-         } else {
-            [tempDocumentItem setDepartment:tempDocumentItem.location.name];
+      if (![[item objectForKey:@"label"] isEqual:[NSNull null]]) {
+         [tempDocumentItem setLabel:[item objectForKey:@"label"]];
+      }
+      
+      if (![[item objectForKey:@"department"] isEqual:[NSNull null]]) {
+         NSDictionary *department = [item objectForKey:@"department"];
+         [tempDocumentItem setUri:[department objectForKey:@"id"]];
+         
+         if (![[department objectForKey:@"id"] isEqual:[NSNull null]]) {
+            [tempDocumentItem setUri:[department objectForKey:@"id"]];
+            
+            [tempDocumentItem setLocation:[locationConnector loadLocationForUri:[department objectForKey:@"id"]]];
+            if (![tempDocumentItem.location.shortname isEqualToString:@""]) {
+               [tempDocumentItem setDepartment:tempDocumentItem.location.shortname];
+            } else {
+               [tempDocumentItem setDepartment:tempDocumentItem.location.name];
+            }
          }
       }
       
-      NSArray *storage = [item elementsForName:@"storage"];
-      if ([storage count] == 1) {
-         [tempDocumentItem setStorage:[[storage objectAtIndex:0] stringValue]];
+      if (![[item objectForKey:@"storage"] isEqual:[NSNull null]]) {
+         NSDictionary *storage = [item objectForKey:@"storage"];
+         [tempDocumentItem setStorage:[storage objectForKey:@"content"]];
       }
       
       tempDocumentItem.services = [[NSMutableArray alloc] init];
-      NSArray *availableItems = [item elementsForName:@"available"];
-      for (GDataXMLElement *availableItem in availableItems) {
-         BADocumentItemElement *tempDocumentItemElement = [[BADocumentItemElement alloc] init];
-         [tempDocumentItemElement setAvailable:YES];
-         [tempDocumentItemElement setType:@"available"];
-         [tempDocumentItemElement setHref:[[availableItem attributeForName:@"href"] stringValue]];
-         if ([[[availableItem attributeForName:@"href"] stringValue] isEqualToString:@""] || [[availableItem attributeForName:@"href"] stringValue] == nil) {
-            [tempDocumentItemElement setOrder:NO];
-         } else {
-            [tempDocumentItemElement setOrder:YES];
-         }
-         [tempDocumentItemElement setService:[[availableItem attributeForName:@"service"] stringValue]];
-         [tempDocumentItemElement setDelay:[[availableItem attributeForName:@"delay"] stringValue]];
-         [tempDocumentItemElement setMessage:[[availableItem attributeForName:@"message"] stringValue]];
-         
-         NSArray *limitation = [availableItem elementsForName:@"limitation"];
-         if ([limitation count] == 1) {
-            [tempDocumentItemElement setLimitation:[[limitation objectAtIndex:0] stringValue]];
-         }
-         
-         [tempDocumentItemElement setExpected:[[availableItem attributeForName:@"expected"] stringValue]];
-         [tempDocumentItemElement setQueue:[[availableItem attributeForName:@"queue"] stringValue]];
-         [tempDocumentItem.services addObject:tempDocumentItemElement];
-      }
-      tempDocumentItem.unavailable = [[NSMutableArray alloc] init];
-      NSArray *unavailableItems = [item elementsForName:@"unavailable"];
-      for (GDataXMLElement *unavailableItem in unavailableItems) {
-         BADocumentItemElement *tempDocumentItemElement = [[BADocumentItemElement alloc] init];
-         [tempDocumentItemElement setAvailable:NO];
-         [tempDocumentItemElement setType:@"unavailable"];
-         [tempDocumentItemElement setHref:[[unavailableItem attributeForName:@"href"] stringValue]];
-         if ([[[unavailableItem attributeForName:@"href"] stringValue] isEqualToString:@""] || [[unavailableItem attributeForName:@"href"] stringValue] == nil) {
-            [tempDocumentItemElement setOrder:NO];
-         } else {
-            [tempDocumentItemElement setOrder:YES];
-         }
-         [tempDocumentItemElement setService:[[unavailableItem attributeForName:@"service"] stringValue]];
-         [tempDocumentItemElement setDelay:[[unavailableItem attributeForName:@"delay"] stringValue]];
-         [tempDocumentItemElement setMessage:[[unavailableItem attributeForName:@"message"] stringValue]];
-         [tempDocumentItemElement setLimitation:[[unavailableItem attributeForName:@"limitation"] stringValue]];
-         [tempDocumentItemElement setExpected:[[unavailableItem attributeForName:@"expected"] stringValue]];
-         [tempDocumentItemElement setQueue:[[unavailableItem attributeForName:@"queue"] stringValue]];
-         [tempDocumentItem.services addObject:tempDocumentItemElement];
-      }
       
+      NSArray *availableItems;
+      if (![[item objectForKey:@"available"] isEqual:[NSNull null]]) {
+         availableItems = [item objectForKey:@"available"];
+         for (NSDictionary *availableItem in availableItems) {
+            BADocumentItemElement *tempDocumentItemElement = [[BADocumentItemElement alloc] init];
+            [tempDocumentItemElement setAvailable:YES];
+            [tempDocumentItemElement setType:@"available"];
+            
+            if (![[availableItem objectForKey:@"href"] isEqual:[NSNull null]]) {
+               [tempDocumentItemElement setHref:[availableItem objectForKey:@"href"]];
+               if ([[availableItem objectForKey:@"href"] isEqualToString:@""]) {
+                  [tempDocumentItemElement setOrder:NO];
+               } else {
+                  [tempDocumentItemElement setOrder:YES];
+               }
+            } else {
+               [tempDocumentItemElement setOrder:NO];
+            }
+            
+            if (![[availableItem objectForKey:@"service"] isEqual:[NSNull null]]) {
+               [tempDocumentItemElement setService:[availableItem objectForKey:@"service"]];
+            }
+            
+            if (![[availableItem objectForKey:@"delay"] isEqual:[NSNull null]]) {
+               [tempDocumentItemElement setDelay:[availableItem objectForKey:@"delay"]];
+            }
+            
+            if (![[availableItem objectForKey:@"message"] isEqual:[NSNull null]]) {
+               [tempDocumentItemElement setMessage:[availableItem objectForKey:@"message"]];
+            }
+            
+            if (![[availableItem objectForKey:@"limitation"] isEqual:[NSNull null]]) {
+               NSArray *limitation = [availableItem objectForKey:@"limitation"];
+               if ([limitation count] >= 1) {
+                  [tempDocumentItemElement setLimitation:[[limitation objectAtIndex:0] objectForKey:@"content"]];
+               }
+            }
+            
+            if (![[availableItem objectForKey:@"expected"] isEqual:[NSNull null]]) {
+               [tempDocumentItemElement setExpected:[availableItem objectForKey:@"expected"]];
+            }
+            
+            if (![[availableItem objectForKey:@"queue"] isEqual:[NSNull null]]) {
+               [tempDocumentItemElement setQueue:[availableItem objectForKey:@"queue"]];
+            }
+            
+            [tempDocumentItem.services addObject:tempDocumentItemElement];
+         }
+      }
+
+      NSArray *unavailableItems;
+      if (![[item objectForKey:@"unavailable"] isEqual:[NSNull null]]) {
+         unavailableItems = [item objectForKey:@"unavailable"];
+         for (NSDictionary *unavailableItem in unavailableItems) {
+            BADocumentItemElement *tempDocumentItemElement = [[BADocumentItemElement alloc] init];
+            [tempDocumentItemElement setAvailable:NO];
+            [tempDocumentItemElement setType:@"unavailable"];
+            
+            if (![[unavailableItem objectForKey:@"href"] isEqual:[NSNull null]]) {
+               [tempDocumentItemElement setHref:[unavailableItem objectForKey:@"href"]];
+               if ([[unavailableItem objectForKey:@"href"] isEqualToString:@""]) {
+                  [tempDocumentItemElement setOrder:NO];
+               } else {
+                  [tempDocumentItemElement setOrder:YES];
+               }
+            } else {
+               [tempDocumentItemElement setOrder:NO];
+            }
+            
+            if (![[unavailableItem objectForKey:@"service"] isEqual:[NSNull null]]) {
+               [tempDocumentItemElement setService:[unavailableItem objectForKey:@"service"]];
+            }
+            
+            if (![[unavailableItem objectForKey:@"delay"] isEqual:[NSNull null]]) {
+               [tempDocumentItemElement setDelay:[unavailableItem objectForKey:@"delay"]];
+            }
+            
+            if (![[unavailableItem objectForKey:@"message"] isEqual:[NSNull null]]) {
+               [tempDocumentItemElement setMessage:[unavailableItem objectForKey:@"message"]];
+            }
+            
+            if (![[unavailableItem objectForKey:@"limitation"] isEqual:[NSNull null]]) {
+               NSArray *limitation = [unavailableItem objectForKey:@"limitation"];
+               if ([limitation count] >= 1) {
+                  [tempDocumentItemElement setLimitation:[[limitation objectAtIndex:0] objectForKey:@"content"]];
+               }
+            }
+            
+            if (![[unavailableItem objectForKey:@"expected"] isEqual:[NSNull null]]) {
+               [tempDocumentItemElement setExpected:[unavailableItem objectForKey:@"expected"]];
+            }
+            
+            if (![[unavailableItem objectForKey:@"queue"] isEqual:[NSNull null]]) {
+               [tempDocumentItemElement setQueue:[unavailableItem objectForKey:@"queue"]];
+            }
+            
+            [tempDocumentItem.services addObject:tempDocumentItemElement];
+         }
+      }
+       
       if ([availableItems count] == 0 && [unavailableItems count] == 0) {
          tempDocumentItem.daiaInfoFromOpac = YES;
       }
