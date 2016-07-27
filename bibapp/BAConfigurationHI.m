e43fcd546ed66d049b4245c0656acfd03e7a8dcf
Johannes Schultze <schultze@effective-webwork.de>
2016-07-27 14:27:02 +0200

    DAIA Test URL HI.

--- bibapp/BAConfigurationHI.m
+++ bibapp/BAConfigurationHI.m
@@ -14,7 +14,7 @@
 {
     self.currentBibSearchMaximumRecords = @"20";
     [self.currentBibLocalSearchURLs addObject:[[NSArray alloc] initWithObjects:@"opac-de-hil2", @"Standard-Katalog", @"Lokale Suche", @"Lokale Suche", nil]];
-    [self.currentBibDetailURLs addObject:[[NSArray alloc] initWithObjects:@"http://daia.gbv.de/isil/DE-Hil2", @"Standard-Katalog", nil]];
+    [self.currentBibDetailURLs addObject:[[NSArray alloc] initWithObjects:@"http://xhil.gbv.de:7242/DE-Hil2/daia", @"Standard-Katalog", nil]];
     //[self.currentBibPAIAURLs addObject:[[NSArray alloc] initWithObjects:@"https://paia.gbv.de/isil/DE-Hil2", @"Standard-Katalog", nil]];
     [self.currentBibPAIAURLs addObject:[[NSArray alloc] initWithObjects:@"https://paia.gbv.de/DE-Hil2", @"Standard-Katalog", nil]];
     self.currentBibFeedURL = @"https://www.uni-hildesheim.de/index.php?id=8920&type=100";
