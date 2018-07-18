6b05eb9bc3fd41e331fac9cfcedfa60d306ad9dd
Johannes Schultze <schultze@effective-webwork.de>
2018-07-18 15:35:53 +0200

    Added language selection to options.

---
+++ bibapp/BALanguageTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BALanguageTableViewController.h
+//  bibapp
+//
+//  Created by Johannes Schultze on 18.07.18.
+//  Copyright © 2018 Johannes Schultze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BALanguageTableViewController : UITableViewController
+
+@end
