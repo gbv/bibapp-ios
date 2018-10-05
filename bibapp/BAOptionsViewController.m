a0e505ea51fffc0181bd9c6414b89c918e8979f6
Johannes Schultze <schultze@effective-webwork.de>
2018-10-05 12:17:33 +0200

    Bugfix options view iPhone.

--- bibapp/BAOptionsViewController.m
+++ bibapp/BAOptionsViewController.m
@@ -173,7 +173,7 @@
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if (section == 1) {
+    if (section == 2) {
         if (self.appDelegate.configuration.usePushService) {
             return 3;
         } else {
